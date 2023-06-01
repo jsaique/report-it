f54355f5fb415703b2bad66524d79aebc88caf52
jsaique <jsaique11@gmail.com>
2023-05-31 22:36:50 -0400

    Working on update tickets

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL('sqlite:///data.db')
+db = SQL("sqlite:///data.db")
+
 
 @app.after_request
 def after_request(response):
@@ -28,63 +29,75 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-@app.route('/')
+
+@app.route("/")
 @login_required
 def index():
     # Getting the data issue, description, comments
-    tickets = db.execute('SELECT * FROM tickets WHERE user_id = :user_id', user_id=session['user_id'])
+    tickets = db.execute("SELECT * FROM tickets")
 
-    users = db.execute('SELECT username FROM users WHERE id = :id', id=session['user_id'])
+    return render_template("index.html", tickets=tickets)
 
-    return render_template('index.html', tickets=tickets, users=users)
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     # Clear active sessions
     session.clear()
 
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        confirmation = request.form.get('confirmation')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
 
         # Check id forms are blank
         if not username:
-            return apology('Username in required', 400)
-        
+            return apology("Username in required", 400)
+
         elif not password:
-            return apology('Password required', 400)
-        
+            return apology("Password required", 400)
+
         elif not confirmation:
-            return apology('Must confrim password', 400)
-        
+            return apology("Must confrim password", 400)
+
         # Check if password matched
         elif password != confirmation:
-            return apology('Password did not match')
-        
+            return apology("Password did not match")
+
         # Check password requirements
-        if len(password) < 8 or not re.search(r"\d", password) or not re.search(r"[A-Z]", password):
-            return apology('Password must be at least 8 characters long and contain at least one capital letter and one number', 400)
-        
+        if (
+            len(password) < 8
+            or not re.search(r"\d", password)
+            or not re.search(r"[A-Z]", password)
+        ):
+            return apology(
+                "Password must be at least 8 characters long and contain at least one capital letter and one number",
+                400,
+            )
+
         # Username query
-        user = db.execute('SELECT * FROM users WHERE username = ?', username)
+        user = db.execute("SELECT * FROM users WHERE username = ?", username)
 
         # Check if the user exist
         if len(user) != 0:
-            return apology('User already exist', 400)
-        
+            return apology("User already exist", 400)
+
         # Adding user to data.db
-        db.execute('INSERT INTO users (username, hash) VALUES (?, ?)', username, generate_password_hash(password))
+        db.execute(
+            "INSERT INTO users (username, hash) VALUES (?, ?)",
+            username,
+            generate_password_hash(password),
+        )
 
-        user = db.execute('SELECT * FROM users WHERE username = ?', username)
+        user = db.execute("SELECT * FROM users WHERE username = ?", username)
 
-        # User logged in 
-        session['user_id'] = user[0]['id']
+        # User logged in
+        session["user_id"] = user[0]["id"]
 
-        return redirect('/')
-    
+        return redirect("/")
+
     else:
-        return render_template('register.html')
+        return render_template("register.html")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -95,7 +108,6 @@ def login():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
         # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
@@ -103,13 +115,16 @@ def login():
         # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
-            
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
@@ -121,7 +136,8 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
-    
+
+
 @app.route("/logout")
 def logout():
     """Log user out"""
@@ -132,51 +148,91 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
-@app.route('/create', methods=['GET', 'POST'])
+
+@app.route("/create", methods=["GET", "POST"])
 def create():
-    if request.method == 'POST':
-        issue = request.form.get('issue')
-        description = request.form.get('description')
-        comments = request.form.get('comments')
+    if request.method == "POST":
+        issue = request.form.get("issue")
+        description = request.form.get("description")
+        comments = request.form.get("comments")
 
         # Check if input and textarea are blank
         if not issue:
-            return apology('Issue required', 400)
-        
-        elif not description:
-            return apology('Description required', 400)
-        
-        # Add the ticket to database
-        db.execute('INSERT INTO tickets (user_id, issue, description, comments) VALUES (:user_id, :issue, :description, :comments)', user_id=session['user_id'], issue=issue, description=description, comments=comments)
-        
-        flash(f'A ticket has been created!')
-        return redirect('/')
-    else:
-        return render_template('create.html')
+            return apology("Issue required", 400)
 
-@app.route('/open', methods=['GET', 'POST'])
+        elif not description:
+            return apology("Description required", 400)
+
+        # Add the ticket to database
+        db.execute(
+            "INSERT INTO tickets (user_id, issue, description, comments) VALUES (:user_id, :issue, :description, :comments)",
+            user_id=session["user_id"],
+            issue=issue,
+            description=description,
+            comments=comments,
+        )
+
+        flash(f"A ticket has been created!")
+        return redirect("/")
+    else:
+        return render_template("create.html")
+
+
+@app.route("/open", methods=["GET", "POST"])
 def open():
     # Getting the data issue, description, comments...
-    tickets = db.execute('SELECT * FROM tickets WHERE user_id = :user_id', user_id=session['user_id'])
+    tickets = db.execute(
+        "SELECT * FROM tickets WHERE user_id = :user_id", user_id=session["user_id"]
+    )
 
-    return render_template('open.html', tickets=tickets)
+    return render_template("open.html", tickets=tickets)
 
-@app.route('/update', methods=['GET', 'POST'])
+
+@app.route("/update", methods=["GET", "POST"])
 def update():
-    action = request.form.get('action')
+    if request.method == "GET":
+        ticket_id = request.args.get("ticket_id")
+        tickets = db.execute("SELECT * FROM tickets WHERE id = ?", (ticket_id,))
 
-    if action == 'update':
-        return apology('TODO')
-    
-    elif action == 'resolve':
-        return apology('TODO')
+    action = request.form.get("action")
+    ticket_id = request.form.get("ticket_id")
 
-    return render_template('update.html')
+    if action == "update":
+        # Get the updated issue, description, and comments from the form
+        updated_comments = request.form.get("comments")
 
-@app.route('/closed')
+        # Update the ticket in the database
+        db.execute(
+            "UPDATE tickets SET comments = ? WHERE id = ?",
+            (updated_comments, ticket_id),
+        )
+
+        flash("Ticket updated!")
+        # Redirect to a success page or display a success message
+        return redirect("/")
+
+    elif action == "resolve":
+        # Update the ticket's state to 'close' in the database
+        db.execute(
+            "UPDATE tickets SET state = 'close' WHERE id = ?",
+            (ticket_id,),
+        )
+
+        flash("Ticket resolved!")
+        # Redirect to the ticket details page or display a success message
+        return redirect("/", ticket_id=ticket_id)
+
+    return render_template("update.html", tickets=tickets)
+
+
+@app.route("/closed")
 def close():
-    return apology('TODO')
+    return apology("TODO")
 
-@app.route('/history')
+
+@app.route("/history")
 def history():
-    return apology('TODO')
+    tickets = db.execute(
+        "SELECT * FROM tickets WHERE user_id = :user_id", user_id=session["user_id"]
+    )
+    return apology("TODO")
